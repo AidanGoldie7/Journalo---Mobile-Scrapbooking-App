81aafd62bf8e7ede6a47a579884c7e890a08c672
aidangoldie7 <40402670@live.napier.ac.uk>
2022-03-16 14:05:05 +0000

    4. Journals -> Home navigation

--- app/src/main/java/com/example/dissapp/JournalsActivity.java
+++ app/src/main/java/com/example/dissapp/JournalsActivity.java
@@ -2,7 +2,11 @@ package com.example.dissapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class JournalsActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class JournalsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journals);
+
+
+        //assign button variable to created button
+        Button button = (Button)findViewById(R.id.btn_home);
+
+
+        //on button click
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //click handling code
+                openActivityHome();
+            }
+        });
+    }
+
+    public void openActivityHome(){
+        //intent to navigate to journals page
+        Intent intent = new Intent(JournalsActivity.this, HomeActivity.class);
+        startActivity(intent);
+
+        //display toast message to inform of relocation to journals page
+        Toast.makeText(this, "Home Page", Toast.LENGTH_SHORT).show();
     }
 }
