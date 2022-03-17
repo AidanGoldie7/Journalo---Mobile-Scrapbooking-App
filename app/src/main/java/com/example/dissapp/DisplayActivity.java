338d02466125d82c8efcaa49a429e44b5714f8d7
aidangoldie7 <40402670@live.napier.ac.uk>
2022-03-17 14:23:31 +0000

    8. MVP drawable folder recyclerView

--- app/src/main/java/com/example/dissapp/DisplayActivity.java
+++ app/src/main/java/com/example/dissapp/DisplayActivity.java
@@ -1,14 +1,40 @@
 package com.example.dissapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class DisplayActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    RecyclerView.Adapter placesAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
+    String[] placesNameList = {"Eiffel Tower", "City"};
+    String[] placesDescriptionList = {"pic of eiffel tower", "pic of city"};
+
+    int[] placesImages = {R.drawable.paris, R.drawable.paris1};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        //name can be changed later
+        recyclerView = findViewById(R.id.rvPlaces);
+
+        //use this if changes in the content do not change the recycler view
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+
+        placesAdapter = new PlacesAdapter(this, placesNameList, placesDescriptionList, placesImages);
+        recyclerView.setAdapter(placesAdapter);
+
     }
 }
---
+++ app/src/main/java/com/example/dissapp/PlacesAdapter.java
@@ -0,0 +1,64 @@
+package com.example.dissapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.ViewHolder> {
+
+    Context context;
+    String[] placesNameList;
+    String[] placesDescriptionList;
+    int[] images;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView rowName;
+        TextView rowDescription;
+        ImageView rowImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            rowName = itemView.findViewById(R.id.textView1);
+            rowDescription = itemView.findViewById(R.id.textView2);
+            rowImage = itemView.findViewById(R.id.imageView);
+        }
+    }
+
+
+    public PlacesAdapter(Context context, String[] placesNameList, String[] placesDescriptionList, int[] images){
+        this.context = context;
+        this.placesNameList = placesNameList;
+        this.placesDescriptionList = placesDescriptionList;
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public PlacesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.single_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(@NonNull PlacesAdapter.ViewHolder holder, int position) {
+        holder.rowName.setText(placesNameList[position]);
+        holder.rowDescription.setText(placesDescriptionList[position]);
+        holder.rowImage.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return placesNameList.length;
+    }
+}
